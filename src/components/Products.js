2bbb19165d032f8ad3fcd543a9357d42652a6f2a
unknown <gargsoffia@gmail.com>
2021-07-13 20:50:19 +0530

    add comments

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,58 +1,66 @@
-import axios from 'axios';
-import React from 'react';
+import axios from "axios";
+import React from "react";
 
-class Product extends React.Component{
-    state={
-        products:[],
-        selectedProducts :[] //array of ids
+class Product extends React.Component {
+  /**
+   *  Manage the state for the total and selected products
+   */
+  state = {
+    products: [],
+    selectedProducts: [], //array of ids
+  };
+  // get the data on component load
+  componentDidMount = async () => {
+    const headers = {
+      "Content-Type": "text/plain",
+    };
+    const result = await axios.get("http://localhost:3000/products", {
+      headers,
+    });
+    if (result && result.status === 200) {
+      this.setState({
+        products: result.products,
+      });
     }
-    componentDidMount= async()=>{
-        const headers = {
-            'Content-Type': 'text/plain'
-        };
-       const result = await axios.get('http://localhost:3000/products',{headers})
-       if(result && result.status === 200){
-        this.setState({
-            products:result.products
-        })
-       }
-    }
+  };
+  /**
+   *
+   * Listeners for Adding and Removing Product
+   */
+  addProductHandler = (product) => {
+    let selectedProducts = [...this.state.selectedProducts, product.id];
+    this.setState({
+      selectedProducts,
+    });
+  };
 
-    addProductHandler = (product)=>{
-        let selectedProducts = [...this.state.selectedProducts,product.id]
-         this.setState({
-             selectedProducts
-         })
-    }
+  removeProductHandler = (product) => {
+    var updatedProducts = this.state.products.filter((productId) => {
+      return productId != product.id;
+    });
+    this.setState({
+      selectedProducts: updatedProducts,
+    });
+  };
 
-    removeProductHandler = (product)=>{
-        var updatedProducts = this.state.products.filter((productId)=>{
-            return productId!= product.id
-        })
-        this.setState({
-            selectedProducts:updatedProducts
-        })
-    }
-
-    render(){
-
-        const prroductList = this.state.products.map((product)=>{
-            return (
-                <div>
-                    <h1>ProducT Name := {product.name}</h1>
-                    <h2>Proce := {product.price} Including GST</h2>
-                    <button onClick = {addProductHandler(product)}>Add</button>
-                    <button onClick = {removeProductHandler}>Remove</button>
-                </div>
-            )
-        })
-        return (
-           <div>
-               Choose your favourite Products
-               {productList}
-           </div>
-        )
-    }
+  render() {
+    const prroductList = this.state.products.map((product) => {
+      return (
+        <div>
+          <h1>ProducT Name := {product.name}</h1>
+          <h2>Proce := {product.price} Including GST</h2>
+          <button onClick={addProductHandler(product)}>Add</button>
+          <button onClick={removeProductHandler}>Remove</button>
+        </div>
+      );
+    });
+    return (
+      <div>
+        Choose your favourite Products
+        {productList}
+      </div>
+    );
+  }
 }
 
-export default Product
+export default Product;
